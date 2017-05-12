99212d1cdaf94b1708848d4dc5324da2d2cf010d
Alex Lim <onetoday1030@gmail.com>
2017-05-12 16:44:52 -0400

    using process.env.PORT

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,6 +1,31 @@
+import path from 'path';
 import nextRoutes from 'next-routes';
-const routes = module.exports = nextRoutes();
+import { template } from 'lodash';
+import fs from 'fs';
 
+<<<<<<< HEAD
 routes.add('event', '/:collectiveSlug/events/:eventSlug');
 routes.add('events', '/:collectiveSlug/events');
 routes.add('events', '/');
+=======
+const pages = nextRoutes();
+
+pages.add('event', '/:collectiveSlug/events/:eventSlug');
+pages.add('button', '/:collectiveSlug/donate/button');
+
+module.exports = (server) => {
+
+  server.get('/:collectiveSlug/donate/button.js', (req, res) => {
+    const content = fs.readFileSync(path.join(__dirname,'../templates/widget.js'), 'utf8');
+    const compiled = template(content);
+    res.setHeader('content-type', 'application/javascript');
+    res.send(compiled({
+      collectiveSlug: req.params.collectiveSlug,
+      host: 'http://localhost:3000'
+    }))
+  });
+
+  return pages.getRequestHandler(server.next);
+
+}
+>>>>>>> 2a3fde1... new /:collectiveSlug/donate/button.js route
