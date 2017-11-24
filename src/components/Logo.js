c4eda3a6dd945f0a7390d60140aff69221a102d2
Alex Lim <onetoday1030@gmail.com>
2017-11-25 00:08:36 +0100

    add incurredAt, fix sorting of collectives, fix css

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,6 +3,10 @@ import { imagePreview } from '../lib/utils';
 
 export default ({ src, style = {}, height }) => {
   style.height = style.height || height;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 003ca78... Connect Twitter/Github/Stripe
   const image = imagePreview(src, defaultImage.ORGANIZATION, { height: style.height });
   return (
     <img className="logo" src={image} style={style} />
