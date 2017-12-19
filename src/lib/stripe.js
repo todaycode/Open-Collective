d2ba0d23eab73e43daa8da91160309591fa919cb
Alex Lim <onetoday1030@gmail.com>
2017-12-19 15:45:21 -0500

    fix error invalid value with floats

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,17 +1,31 @@
 import Payment from 'payment';
 
-const getStripeToken = (card) =>
-new Promise((resolve, reject) => {
+const getStripeToken = (type = 'cc', data) => {
+
   // eslint-disable-next-line
-  Stripe.card.createToken(card, (status, res) => {
-    if (res.error) {
-      reject(res.error.message);
-    } else {
-      console.log(">>> Stripe createToken result", res)
-      resolve({token: res.id, card: res.card});
-    }
-  });
-});
+  switch (type) {
+    case 'cc': // credit card
+      return stripe.createToken(data).then(res => {
+        console.log(">>> stripe createToken result", res)
+        return { token: res.token.id, card: res.token.card };
+      });
+      break;
+
+    case 'btc': // bitcoin
+      return stripe.createSource({
+        type: 'bitcoin',
+        amount: data.amount,
+        currency: 'usd',
+        metadata: data.metadata,
+        owner: {
+          email: data.email,
+          name: data.name
+        }
+      }).then(function(res) {
+        return { token: res.source.id, data: res.source.bitcoin };
+      });
+  }
+}
 
 const isValidCard = (card) => {
   if (typeof card.cvc !== 'string') {
